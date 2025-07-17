0e0002bfb1c2f7aa87a65c4baf76f7a7062ce500
Josue <jowebdesigc@gmail.com>
2025-07-17 12:52:59 -0400

    red

--- types/perfume.ts
+++ types/perfume.ts
@@ -2,12 +2,14 @@ export interface Perfume {
   id: string;
   name: string;
   brand: string;
-  price: number;
-  originalPrice?: number;
+  sizes: {
+    size: string;
+    price: number;
+    originalPrice?: number;
+  }[];
   description: string;
   image: string;
   category: 'masculino' | 'femenino' | 'unisex';
-  size: string;
   notes: {
     top: string[];
     heart: string[];
