fc66252a914dce4e3cd5d1adb7a969b4ee8e9fa4
Josue <jowebdesigc@gmail.com>
2025-07-17 16:25:27 -0400

    asdsafasfd

--- components/PerfumeCardKit.tsx
+++ components/PerfumeCardKit.tsx
@@ -1,7 +1,6 @@
 'use client';
 
 import { useState } from 'react';
-import { Perfume } from '@/types/perfume';
 import { useCart } from '@/contexts/CartContext';
 import { Card, CardContent } from '@/components/ui/card';
 import { Badge } from '@/components/ui/badge';
