85fb9c5711cd1130f7ad27d74dc3b1bf8a3a52ae
Josue <jowebdesigc@gmail.com>
2025-07-21 16:12:10 -0400

    asdl

--- sanity/schemaTypes/index.ts
+++ sanity/schemaTypes/index.ts
@@ -1,8 +1,5 @@
-import { type SchemaTypeDefinition } from 'sanity'
 import perfume from './perfume'
 import category from './category'
 import brand from './brand'
 
-export const schema: { types: SchemaTypeDefinition[] } = {
-  types: [perfume, category, brand],
-}
+export const schema = [perfume, category, brand]
